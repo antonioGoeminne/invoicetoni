6dc5d0fa4bc99b684e7bb660251979befa4c74d0
antonionicolas___ <antonionicolas3003@gmail.com>
2024-02-25 18:43:05 -0300

    FEAT: removing unnecesarys menus and revalidating page with refresh data every 60 seconds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,26 @@
-import { Menu } from "@/features/ui";
 import styles from "./page.module.css";
 import { WrapperInvoices } from "@/features/invoices/components/wrapper-invoices";
 import { DrawerInvoice } from "@/features/invoices/components/drawer-invoice";
 import { Suspense } from "react";
+import { getInvoices } from "@/features/api";
 
-export default function Home() {
+export default async function Home() {
+  const invoices = await getInvoices();
+
   return (
     <main className={styles.main}>
       <div className={styles.topBar}>
         <div>
           <h2>Invoices</h2>
-          <p className={styles.subText}>There are 7 total invoices</p>
-        </div>
-        <div className={styles.flex} style={{ gap: 15 }}>
-          <Menu options={["Latest first", "Older first"]} label="Sort" />
-          <Menu
-            options={["Paid", "Pending", "Draft", "Cancelled"]}
-            label="Filter by status"
-          />
+          <p className={styles.subText}>
+            There are {invoices?.length || 0} total invoices
+          </p>
         </div>
         <Suspense>
           <DrawerInvoice />
         </Suspense>
       </div>
-      <WrapperInvoices />
+      <WrapperInvoices invoices={invoices} />
     </main>
   );
 }
--- src/features/api/get-invoices.tsx
+++ src/features/api/get-invoices.tsx
@@ -1,13 +1,11 @@
-
-import { BASE_URL } from "@/config"
+import { BASE_URL } from "@/config";
 
 export async function getInvoices() {
-    const res = await fetch(BASE_URL + 'invoices')
-   
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-   
-    return res.json()
+  const res = await fetch(BASE_URL + "invoices", { next: { revalidate: 60 } });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
-   
+
+  return res.json();
+}
